6e75eb982c46e0396b0f73563fb77303c66d9069
Denuka Dissanayaka <www.denukadissanayaka@gmail.com>
2022-07-31 10:49:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, Button, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@mui/material';
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
+import useStyles from './styles';
+
+const cards = [1,2,3,4,5,6,7,8,9]
 
 function App() {
+	const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+		<CssBaseline/>
+		<AppBar position='relative'>
+			<Toolbar>
+				<PhotoCameraIcon className={classes.icon}/>
+				<Typography variant='h6'>
+					Photo Album
+				</Typography>
+			</Toolbar>
+		</AppBar>
+		<main>
+			<div className={classes.container}>
+				<Container maxWidth='sm'>
+					<Typography variant='h2' align='center' color='textPrimary' gutterBottom> Photo Album</Typography>
+					<Typography variant='h5' align='center' color='textSecondary' paragraph>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis ipsam aliquid, ex quam esse velit repellendus accusantium dolorem expedita asperiores corrupti est? Molestiae animi temporibus doloremque est laborum excepturi sint!
+					</Typography>
+					<div className={classes.button}>
+						<Grid container spacing={2} justifyContent='center'>
+							<Grid item>
+								<Button variant="contained">See my photos</Button>
+							</Grid>
+							<Grid item>
+								<Button variant="outlined">Secondaty action</Button>
+							</Grid>
+						</Grid>
+					</div>
+				</Container>
+			</div>
+			<Container className={classes.cardGrid} maxWidth="md">
+				<Grid container spacing={4}>
+					{cards.map((card) => (
+
+					<Grid item key={card} xs={12} sm={6} md={4}>
+						<Card className={classes.card}>
+							<CardMedia
+								className={classes.CardMedia}
+								image="https://source.unsplash.com/random"
+								title="Image title"
+							/>
+							<CardContent className={classes.CardContent}>
+								<Typography gutterBottom variant='h5'>
+									Heading
+								</Typography>
+								<Typography gutterBottom variant='h5'>
+									This is a media card. You can use this section to describe the content.
+								</Typography>
+							</CardContent>
+							<CardActions>
+								<Button size="small" color='primary'>View</Button>
+								<Button size="small" color='primary'>Edit</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+					))}
+				</Grid>
+			</Container>
+		</main>
+		<footer className={classes.footer}>
+			<Typography variant='h6' align='center' gutterBottom>
+				Footer
+			</Typography>
+			<Typography variant='subtitle1' align='center' color='textSecondary' gutterBottom>
+				Facebook | Instagram | Youtube | Twitter
+			</Typography>
+		</footer>
+    </>
   );
 }
 
